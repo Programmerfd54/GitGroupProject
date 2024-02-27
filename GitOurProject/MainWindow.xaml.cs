a4637b41c39df048f0c45ac9eb4ccb981e9493f8
Programmerfd54 <99553384+Programmerfd54@users.noreply.github.com>
2024-02-28 00:12:44 +0300

    Update MainWindow.xaml.cs

--- GitOurProject/MainWindow.xaml.cs
+++ GitOurProject/MainWindow.xaml.cs
@@ -20,77 +20,11 @@ namespace GitOurProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string currentOperation = ""; // Текущая операция
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Equals_Click(object sender, RoutedEventArgs e)
-        {
-            PerformOperation();
-        }
-        double result = 0;
-        private void PerformOperation()
-        {
-            // Проверяем наличие чисел и выбранной операции
-            if (string.IsNullOrWhiteSpace(currentOperation))
-            {
-                MessageBox.Show("Выберите операцию!");
-                return;
-            }
-
-            if (string.IsNullOrWhiteSpace(log.Text) || string.IsNullOrWhiteSpace(log2.Text))
-            {
-                MessageBox.Show("Введите числа!");
-                return;
-            }
-
-
-            // Парсинг чисел из текстовых полей
-            if (double.TryParse(log.Text, out double num1) && double.TryParse(log2.Text, out double num2))
-            {
-
-
-                // Выполнение операции в соответствии с текущей выбранной операцией
-                switch (currentOperation)
-                {
-                    case "+":
-                        result = num1 + num2;
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        break;
-                    case "/":
-                        if (num2 != 0)
-                            result = num1 / num2;
-                        else
-                        {
-                            MessageBox.Show("Деление на ноль невозможно!");
-                            return;
-                        }
-                        break;
-                    case "√":
-                        if (num2 >= 0)
-                            result = Math.Sqrt(num2);
-                        else
-                        {
-                            MessageBox.Show("Извлечение корня из отрицательного числа невозможно!");
-                            return;
-                        }
-                        break;
-                    default:
-                        MessageBox.Show("Выберите операцию!");
-                        return;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Введите корректные числа!");
-            }
-        }
+        
     }
 }
